2854b026258ee14c945417cda161a35f7fa82ca7
Ricardinho <andrade.comim@gmail.com>
2021-01-07 18:44:54 -0300

    escopo de variavel var e let

---
+++ escopoDeVariavel.js
@@ -0,0 +1,11 @@
+for (var i = 0; i< 10; i++){
+    console.log(i)
+}
+console.log('i =', i)
+
+console.log('=========================================')
+
+for (let a = 0; a < 10; a++){
+    console.log(a)
+}
+console.log('a =', a)
