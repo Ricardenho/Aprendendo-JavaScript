898f75905e92cc0f8565ff4c19b81b24680de2d1
Ricardinho <andrade.comim@gmail.com>
2021-01-07 16:54:23 -0300

    bolleanos

---
+++ bollean.js
@@ -0,0 +1,23 @@
+let x =  false
+console.log(x)
+x = true
+console.log(x)
+
+//valores verdadeiros
+console.log('=========================================')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!"texto")
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(x = true))
+console.log('=========================================')
+//os Falsos
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(x = false))
