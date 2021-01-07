4d09fa08571a3182447483209b7d856a1dee3f5b
Ricardinho <andrade.comim@gmail.com>
2021-01-07 14:20:49 -0300

    variaveis var, let, const

---
+++ variaiveis.js
@@ -0,0 +1,20 @@
+var a = 3
+let b = 4
+console.log(a, b)
+
+/*
+var a = 6
+let b = 9
+
+erro na linha 7, o let não deixa vc re-declarar variaveis
+o var deixa
+*/
+
+var a = 6
+b = 9
+
+console.log(a, b)
+
+const c = 5
+//c = 50 daria erro, pq não se pode alterar o valor de uma const
+console.log(c)
