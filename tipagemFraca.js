aea5f3ce815fecd9d1e0120ac25b067821ceac27
Ricardinho <andrade.comim@gmail.com>
2021-01-07 14:30:07 -0300

    Tipos primitivos

---
+++ tipagemFraca.js
@@ -0,0 +1,29 @@
+let qualquer = 'Bom dia'
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log('----------------------------------------------------------')
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log('----------------------------------------------------------')
+
+qualquer = 2
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log('----------------------------------------------------------')
+
+qualquer = null
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log('----------------------------------------------------------')
+
+qualquer = undefined
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log('----------------------------------------------------------')
+
+qualquer = Array
+console.log(qualquer)
+console.log(typeof qualquer)
+console.log('----------------------------------------------------------')
