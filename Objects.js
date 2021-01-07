d54f14f878128ce9b744631049b96b8784e25b3c
Ricardinho <andrade.comim@gmail.com>
2021-01-07 17:41:40 -0300

    Object Basic

---
+++ Objects.js
@@ -0,0 +1,13 @@
+let prod1 = {}
+prod1.nome = 'Celular mega'
+prod1.preco = 4998.90
+
+console.log(prod1)
+
+const prod2 = {
+    nome: "Camisa Polo",
+    preco : 79.90 
+}
+console.log(prod2)
+
+console.log(prod2.preco.toFixed(2))
