dc7bbb6cb3cd03b4be8a0b21838d35ccee757c78
Ricardinho <andrade.comim@gmail.com>
2021-01-07 20:16:47 -0300

    Operador Ternário

---
+++ operadorTernario.js
@@ -0,0 +1,6 @@
+const result = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+//result recebe nota de parametro
+//se nota for maior ou iagual a 7, retorne 'Aprovado', senão 'Reprovado'
+
+console.log(result(7.1))
+console.log(result(6.9))
