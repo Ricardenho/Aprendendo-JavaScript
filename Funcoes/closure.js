eeb026ebf375908a8cf3235e6f895f0c6476af10
Ricardinho <andrade.comim@gmail.com>
2021-01-11 15:35:15 -0300

    Closure

---
+++ Funcoes/closure.js
@@ -0,0 +1,17 @@
+//Closure é o escopo criado quando uma função é declarada
+//Esse escopopermite a função acessar e manipular variáveis externas á função
+
+const x = 'Global'
+
+function fora (){
+    const x = 'Local'
+    function dentro(){
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
+
+console.log(fora())
