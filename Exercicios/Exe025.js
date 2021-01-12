27949ff605c03fbbdfb5c01f324bf809e7275f93
Ricardinho <andrade.comim@gmail.com>
2021-01-12 18:42:36 -0300

    Exe025

---
+++ Exercicios/Exe025.js
@@ -0,0 +1,11 @@
+/*Escrever um programa para exibir os números de 1 até 50 na tela.*/
+
+function mostraNum(){
+    let cont = 1
+    while (cont <= 50){
+        console.log(cont)
+        cont++
+    }
+}
+
+mostraNum()
