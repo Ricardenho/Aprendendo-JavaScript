b293e3ca8d80a587ffba4c83815f14491999932e
Ricardinho <andrade.comim@gmail.com>
2021-01-15 16:28:34 -0300

    Reduce

---
+++ Arrays/reduce.js
@@ -0,0 +1,25 @@
+const alunos = [
+    {nome: 'João', nota: 7.3, bolsista: false},
+    {nome: 'Maria', nota: 9.2, bolsista: true},
+    {nome: 'Pedro', nota: 9.8, bolsista: false},
+    {nome: 'Ana', nota: 8.7, bolsista: true},
+]
+
+console.log(alunos.map(a => a.nota))
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
+    console.log(acumulador, atual)
+    return acumulador + atual
+}, 10)
+
+console.log(resultado)
+
+//==========================================================================================
+console.log('===========================================================================')
+
+//Todos os alunos são bolsistas?
+const todosBolsista = (resultado, bolsista) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsista))
+
+//Algum aluno é bolsista?
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
